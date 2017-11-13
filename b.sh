dc93fbfe579c77b0e1637bc96eb5261be68d0f4b
mhtran <hoangtm@runsystem.net>
2017-11-13 09:22:46 +0700

    update .gitignore, b.sh

--- b.sh
+++ b.sh
@@ -1,6 +1,8 @@
+git remote add origin https://github.com/mhtranbn/RSUtils.git
+git push -u origin master --tags
 pod repo add RSUtils https://github.com/mhtranbn/RSUtils.git
 pod repo push RSUtils RSUtils.podspec --allow-warnings
 pod lib lint --allow-warnings
 pod repo update
-pod trunk push RSUtils.podspec --allow-warnings
-
+pod trunk register hoangtm@runsystem.net 'hoangtm' --description='macbook pro'
+pod trunk push RSUtils.podspec --allow-warnings --verbose
