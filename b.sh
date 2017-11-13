a1304b6c5336515156f0b3a3c2b99cc3ead7ed9c
mhtran <hoangtm@runsystem.net>
2017-11-13 07:48:01 +0700

    update b.sh

--- b.sh
+++ b.sh
@@ -1,2 +1,6 @@
+pod repo add RSUtils https://github.com/mhtranbn/RSUtils.git
+pod repo push RSUtils RSUtils.podspec --allow-warnings
+pod lib lint --allow-warnings
+pod repo update
 pod trunk push RSUtils.podspec --allow-warnings
 
